c1252ed576c3d2319b4ff34372c237857b9afea4
Betty Mutai <bettymutai00@gmail.com>
2017-08-31 16:26:27 +0300

    Added a column user_id to books

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831094522) do
+ActiveRecord::Schema.define(version: 20170831115304) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 20170831094522) do
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
