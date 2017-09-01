4ccf63167fc15992474939ca1b66b8b011c8dfd2
Betty Mutai <bettymutai00@gmail.com>
2017-09-01 13:36:01 +0300

    Added jquery to truncate the description and show more or less on click

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,49 @@
 // about supported directives.
 //
 //= require rails-ujs
+//= require jquery
+//= require jquery_ujs
 //= require turbolinks
 //= require_tree .
-//= require jquery
-//= require toastr
 //= require bootstrap-sprockets
+//= require toastr
+document.addEventListener("turbolinks:load", function() {
+ my_func();
+})
+function my_func() {
+$(document).ready(function() {
+(function(){
+  	var showChar = 50;
+    var ellipsestext = "...";
+
+	$('.truncate').each(function() {
+        var content = $(this).html();
+        if(content.length > showChar) {
+
+            var c = content.substr(0, showChar);
+ 			var h = content;
+            var html = '<div class="truncate-text" style="display:block">' + c + '<span class="moreellipses">' + ellipsestext + '&nbsp;&nbsp;<a href="" class="moreless more">more</a></span></span></div><div class="truncate-text" style="display:none">' + h + '<a href="" class="moreless less">Less</a></span></div>';
+
+            $(this).html(html);
+        }
+
+    });
+
+    $(".moreless").click(function(){
+		var thisEl = $(this);
+        if(thisEl.hasClass("less")) {
+        	thisEl.closest('.truncate-text').prev('.truncate-text').toggle();
+    		thisEl.closest('.truncate-text').slideToggle();
+        } else {
+			thisEl.closest('.truncate-text').toggle();
+			thisEl.closest('.truncate-text').next('.truncate-text').fadeToggle();
+        }
+	   	return false;
+    });
+	/* end iffe */
+	}());
+
+/* end ready */
+});
+
+}
