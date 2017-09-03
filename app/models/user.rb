5d66d0a3e169107ba2a2278dbf1d5ce57a7622e4
Betty Mutai <bettymutai00@gmail.com>
2017-09-03 17:47:48 +0300

    Added pictureuploader for profile picture

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :books
   has_many :comments
+  mount_uploader :picture, PictureUploader
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
