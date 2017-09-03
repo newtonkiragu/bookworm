05a6d040a692ccbc4558776032327ceddf59a5cd
Betty Mutai <bettymutai00@gmail.com>
2017-09-03 15:00:14 +0300

    nested upvotes and downvotes in resource books

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,13 @@ Rails.application.routes.draw do
       passwords: 'users/passwords',
       registrations: 'users/registrations'
   }
-  resources :books
+  resources :books do
+    resources :comments
+    member do
+     put "like", to: "books#upvote"
+     put "dislike", to: "books#downvote"
+   end
+  end
   resources :users
   root 'books#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
