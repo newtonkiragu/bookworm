777753b6581721863e6b393b6369c6aa59eeb50e
Betty Mutai <bettymutai00@gmail.com>
2017-09-03 09:20:56 +0300

    Added the search class method to Book model

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,13 @@
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :user
+  include Filterable
+
+  def self.search(search)
+   if search
+    where('category LIKE ?', "%#{search}%")
+   else
+    unscoped
+   end
+  end
 end
