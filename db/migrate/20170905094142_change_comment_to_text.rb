1df7e5beb8a2dbecf783ab94662bd84cae8c2482
Betty Mutai <bettymutai00@gmail.com>
2017-09-05 12:44:11 +0300

    Changed comments datatype from string to text

---
+++ db/migrate/20170905094142_change_comment_to_text.rb
@@ -0,0 +1,5 @@
+class ChangeCommentToText < ActiveRecord::Migration[5.1]
+  def change
+      change_column :comments, :comment, :text
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903194111) do
+ActiveRecord::Schema.define(version: 20170905094142) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,7 @@ ActiveRecord::Schema.define(version: 20170903194111) do
   end
 
   create_table "comments", force: :cascade do |t|
-    t.string "comment"
+    t.text "comment"
     t.integer "user_id"
     t.integer "book_id"
   end
