86ff30cbfe5ec99cdeb0c0a37a1a3e2d7df02638
kiburei <akiburei@gmail.com>
2017-09-05 11:10:39 +0300

    add confrimable to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170903194111) do
+ActiveRecord::Schema.define(version: 20170905080623) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,19 +35,6 @@ ActiveRecord::Schema.define(version: 20170903194111) do
     t.integer "book_id"
   end
 
-  create_table "comments_tables", force: :cascade do |t|
-    t.string "comment"
-    t.integer "user_id"
-    t.integer "book_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "reads", force: :cascade do |t|
-    t.boolean "read"
-    t.integer "user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "email", default: "", null: false
@@ -66,6 +53,10 @@ ActiveRecord::Schema.define(version: 20170903194111) do
     t.boolean "superadmin_role", default: false
     t.boolean "supervisor_role", default: false
     t.boolean "user_role", default: true
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
