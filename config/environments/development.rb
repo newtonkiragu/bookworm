9e1cfe4d748906b6aaa06eeb2787fa57434393d6
kiburei <akiburei@gmail.com>
2017-09-05 16:39:10 +0300

    Add new email for bookworm

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,10 +27,21 @@ Rails.application.configure do
   end
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.default_url_options = { host:'localhost', port: '3000' }
+  config.action_mailer.default :charset => "utf-8"
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    enable_starttls_auto: true,
+    domain: "localhost:3000",
+    address: "smtp.gmail.com",
+    authentication: "plain",
+    user_name: "bookwormkenya@gmail.com",
+    password: "bookworm2017",
+    port: 587
 
-  config.action_mailer.perform_caching = false
-
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
@@ -52,5 +63,5 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
-  
+
 end
