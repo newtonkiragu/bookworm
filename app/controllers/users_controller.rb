48f968746a7238e8d47bef8298d6ad443fb46f0a
Betty Mutai <bettymutai00@gmail.com>
2017-09-14 23:36:39 +0300

    Added author to display authors  profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,25 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:author]
+
   def show
     add_reading
     @user = current_user
     @book = current_user.books
   end
+
+  def author
+    @book.user_id = @book.author
+    @user = @author
+    @books = @author.books
+  end
+
+  private
+  def set_user
+    @author = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:id)
+  end
+
 end
