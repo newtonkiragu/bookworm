b084ea75c081312332808499df1d1b84266c614e
Betty Mutai <bettymutai00@gmail.com>
2017-09-14 15:48:00 +0300

    Defined user in show to display profile photo

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -43,6 +43,7 @@ class BooksController < ApplicationController
   # GET /books/1
   # GET /books/1.json
   def show
+    @user = current_user
     @chapters = @book.chapters
     @book = Book.find(params[:id])
     impressionist(@book, "message...") # 2nd argument is optional
