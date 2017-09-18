145355f5df89d63b13f748f8b138412a56388885
Betty Mutai <bettymutai00@gmail.com>
2017-09-18 12:23:31 +0300

    Fixed a bug in removing books from reading list

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -100,11 +100,15 @@ class BooksController < ApplicationController
   # DELETE /books/1
   # DELETE /books/1.json
   def destroy
-    @book.destroy
-    respond_to do |format|
-      format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
-      format.json { head :no_content }
+    chapters = @book.chapters
+    if chapters.destroy
+      @book.destroy
+      respond_to do |format|
+        format.html { redirect_to books_url, notice: 'Book was successfully destroyed.' }
+        format.json { head :no_content }
+      end
     end
+
   end
 
   def upvote
